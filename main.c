8cb91dd78abad9f38825cd8280f2b377e05fbdbe
ambrosia <ambr7156@gmail.com>
2024-06-29 22:40:03 +0300

    Optimized calculation of intersection points with sphere

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 #define RADIUS 1
 
 #define ASPECT ((double)WIDTH / (double)HEIGHT)
-#define COL_ASPECT (11.0f / 24.0f)
+#define COL_ASPECT (11.0f / 24.0f) // Ratio of symbols in console
 
 #define PI 3.1415926535
 
@@ -26,7 +26,7 @@ static void show(void)
     }
 }
 
-// put cursor to the begining of terminal
+// Puts cursor to the begining of terminal
 static void back(void)
 {
     printf("\x1b[%dD", WIDTH);
@@ -45,20 +45,18 @@ V3d v3d_norm(V3d v)
 
 double sph_intersection(V3d o, V3d n, V3d c, double r)
 {
-    double a = v3d_dot(n, n);
     double p = v3d_dot(o, n) - v3d_dot(n, c);
     double q = v3d_dot(o, o) + v3d_dot(c, c) - 2 * v3d_dot(o, c) - r * r;
-    double d = p * p - a * q;
+    double d = p * p - q;
     if (d < 0.0) return -1.0;
-    d = sqrt(d) / a;
-    p = p / a;
+    d = sqrt(d);
     return -p - d;
 }
 
 static char sphere_draw(V2d uv, V3d o, V3d c, V3d l)
 {
     int color = 0;
-    V3d n = v3d_norm(v3d(1.0, uv.x, uv.y));
+    V3d n = v3d_norm(v3d(1.0, uv.x, uv.y)); // direction of ray
     double t = sph_intersection(o, n, c, RADIUS);
     if (t > 0) {
         V3d v = v3d_norm(v3d_sum(o, v3d_mul(n, v3dd(t))));
@@ -72,15 +70,16 @@ static char sphere_draw(V2d uv, V3d o, V3d c, V3d l)
 int main(void)
 {
     grad_size = strlen(grad) - 2;
-    V3d origin = { -2.0, 0.0, 0.0 }; // Origin
+    V3d origin = { -2.0, 0.0, 0.0 }; // Origin of ray
     V3d center = {  0.0, 0.0, 0.0 }; // Sphere center
 
     for (double t = 0.0; t < 2*PI; t += 0.01) {
         V3d light = v3d_norm(v3d(sin(3*t), cos(3*t), -0.5));
         for (size_t i = 0; i < WIDTH; ++i) {
             for (size_t j = 0; j < HEIGHT; ++j) {
-                V2d uv = v2d((double)(i) / (double)(WIDTH) * 2.0 - 1.0, (double)(j) / (double)(HEIGHT) * 2.0 - 1.0);
-                uv.x *= ASPECT * COL_ASPECT;
+                V2d uv = v2d((double)(i) / (double)(WIDTH) * 2.0 - 1.0,
+                             (double)(j) / (double)(HEIGHT) * 2.0 - 1.0);
+                uv.x *= ASPECT * COL_ASPECT; // Aligns coordinates
 
                 char pixel = sphere_draw(uv, origin, center, light);
                 screen[j][i] = pixel;
